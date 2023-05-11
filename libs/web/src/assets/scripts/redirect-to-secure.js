0b5926715d65c29afb99f5f904ec0a216ad6252e
krizz <christian@o3tt3rli.com>
2023-05-11 04:55:34 +0200

    exclude dev server from secure redirect

--- libs/web/src/assets/scripts/redirect-to-secure.js
+++ libs/web/src/assets/scripts/redirect-to-secure.js
@@ -1,5 +1,9 @@
 (() => {
-    const { href } = location;
+    const { href, port } = location;
+    if (String(port) === '3000') {
+        // dev server
+        return;
+    }
     if (!href.toLowerCase().startsWith('https')) {
         location.href = href.replace(/http/i, 'https');
     }
